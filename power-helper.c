LineageOS/android_hardware_qcom_power

power: Allow device specific hooks for set_interactive
* Some devices may want to implement custom hooks

Change-Id: I9be8023c8134b3c262f27b43a28bec0e14c0fd7f

power-helper.c
@@ -321,6 +321,11 @@ int __attribute__ ((weak)) set_interactive_override(int UNUSED(on))
     return HINT_NONE;
 }
 
+
+#ifdef SET_INTERACTIVE_EXT
+extern void cm_power_set_interactive_ext(int on);
+#endif
+
 void power_set_interactive(int on)
 {
     char governor[80];
@@ -335,6 +340,10 @@ void power_set_interactive(int on)
         /* Send Display ON hint to perf HAL */
         perf_hint_enable(VENDOR_HINT_DISPLAY_ON, 0);
     }
+
+#ifdef SET_INTERACTIVE_EXT
+    cm_power_set_interactive_ext(on);
+#endif
 
     if (set_interactive_override(on) == HINT_HANDLED) {
         return;

DotOS/platform_hardware_qcom_power

power-helper.c
@@ -322,6 +322,10 @@ int __attribute__ ((weak)) set_interactive_override(int UNUSED(on))
     return HINT_NONE;
 }
 
+#ifdef SET_INTERACTIVE_EXT
+extern void power_set_interactive_ext(int on);
+#endif
+
 void power_set_interactive(int on)
 {
     char governor[80];
@@ -336,6 +340,10 @@ void power_set_interactive(int on)
         /* Send Display ON hint to perf HAL */
         perf_hint_enable(VENDOR_HINT_DISPLAY_ON, 0);
     }
+
+#ifdef SET_INTERACTIVE_EXT
+    power_set_interactive_ext(on);
+#endif
 
     if (set_interactive_override(on) == HINT_HANDLED) {
         return;